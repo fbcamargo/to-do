1cf21d91f924593dcaa000cf59ad22cc3cfd2f9d
Felipe <camargo963@gmail.com>
2025-05-24 16:24:50 -0300

    User routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { TYPES } from "../types/types";
+import { container } from "../config/container";
+import { UserController } from "../controllers/UserController";
 
 const router = Router();
 
-// routes
+const userController = container.get<UserController>(TYPES.UserController);
+
+router.post("/user", userController.store.bind(userController));
 
 export default router;
